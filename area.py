477a3100048f61773ef8bedb95c3dffdb8f7d9f1
Nigraum <yan.corintima@hotmail.com>
2022-10-08 08:27:40 -0300

    Create area.py challenge

---
+++ area.py
@@ -0,0 +1,10 @@
+a = float(input())
+b = float(input())
+c = float(input())
+triangleArea = (a * c) / 2
+circleArea = 3.14159 * c ** 2
+trapezeArea = ((a + b) * c) / 2
+squareArea = b ** 2
+rectangleArea = a * b
+print('TRIANGULO: %.3f' %triangleArea)
+print('CIRCULO: %.3f' %circleArea)
