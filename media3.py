0fae626df70d3ee6e3a2ae40e680edd0e9894dcd
Nigraum <yan.corintima@hotmail.com>
2022-10-10 08:30:14 -0300

    Finishing media3.py

--- media3.py
+++ media3.py
@@ -1,6 +1,12 @@
 a = float(input())
 b = float(input())
-if ((a + b) / 2) >= 6:
-    print('aprovado')
-elif a = 0:
+media = (a + b) / 2
+if a == 0:
     print('reprovado')
+elif media >= 6:
+    print('aprovado')
+elif media <= 0.995:
+    print('reprovado')
+else:
+    print('talvez com a sub')
+
