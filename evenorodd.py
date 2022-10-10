1a9044ea1231c4817e8a6db1e4bca9356e2b4b69
Nigraum <yan.corintima@hotmail.com>
2022-10-10 08:11:40 -0300

    Create 2 challenges and even or odd finished

---
+++ evenorodd.py
@@ -0,0 +1,7 @@
+a = int(input())
+if (a % 2) == 0:
+    print(a - 1, end= ' ')
+    print(a + 2)
+else:
+    print(a - 2, end= ' ')
+    print(a + 1)
---
+++ media3.py
@@ -0,0 +1,6 @@
+a = float(input())
+b = float(input())
+if ((a + b) / 2) >= 6:
+    print('aprovado')
+elif a = 0:
+    print('reprovado')
