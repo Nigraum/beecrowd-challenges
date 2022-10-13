9ad244af01a3ce348c1b402ee909f28821a63c0b
Nigraum <yan.corintima@hotmail.com>
2022-10-13 10:50:14 -0300

    Create month.py

---
+++ month.py
@@ -0,0 +1,28 @@
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+a = int(input())
+
+if a == 1:
+    print(months[0])
+if a == 2:
+    print(months[1])
+if a == 3:
+    print(months[2])
+if a == 4:
+    print(months[3])
+if a == 5:
+    print(months[4])
+if a == 6:
+    print(months[5])
+if a == 7:
+    print(months[6])
+if a == 8:
+    print(months[7])
+if a == 9:
+    print(months[8])
+if a == 10:
+    print(months[9])
+if a == 11:
+    print(months[10])
+if a == 12:
+    print(months[11])
+
