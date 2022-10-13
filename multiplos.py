f11d22774efbc36ede7e26dbb2542d4bc1c47334
Nigraum <yan.corintima@hotmail.com>
2022-10-13 10:36:16 -0300

    Fixing multiplos.py

--- multiplos.py
+++ multiplos.py
@@ -1,19 +1,16 @@
 n = input().split()
 a, b = n
-
 a = int(a)
 b = int(b)
 
 if a < b:
-if b % a == 0:
-    print("Sao multiplos")
-else:
-    print("Nao sao multiplos")
+    if b % a == 0:
+        print("Sao multiplos")
+    else:
+        print("Nao sao multiplos")
 
 if a > b:
-if a % b == 0:
-    print("Sao multiplos")
-else:
-    print("Nao sao multiplos")
-
-
+    if a % b == 0:
+        print("Sao multiplos")
+    else:
+        print("Nao sao multiplos")
