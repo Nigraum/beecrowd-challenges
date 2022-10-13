519f9343d8ed97f6ba9de61c2fc4e9a3c7e7b74e
Nigraum <yan.corintima@hotmail.com>
2022-10-13 10:30:23 -0300

    Some changes on multiplos.py

--- multiplos.py
+++ multiplos.py
@@ -4,7 +4,16 @@ a, b = n
 a = int(a)
 b = int(b)
 
+if a < b:
 if b % a == 0:
     print("Sao multiplos")
 else:
     print("Nao sao multiplos")
+
+if a > b:
+if a % b == 0:
+    print("Sao multiplos")
+else:
+    print("Nao sao multiplos")
+
+
