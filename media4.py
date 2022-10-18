8f95ab88039bcb9401998729f400adb1bc14567e
Nigraum <yan.corintima@hotmail.com>
2022-10-17 22:22:32 -0300

    Fixing media4.py

--- media4.py
+++ media4.py
@@ -2,14 +2,17 @@ noteAll = input().split()
 n1, n2, n3, n4 = noteAll
 
 media = ((float(n1) * 2) + (float(n2) * 3) + (float(n3) * 4) + (float(n4) * 1)) / 10
-print('Media: %.1f' %media)
+
 if media >= 7:
+    print('Media: %.1f' %media)
     print('Aluno aprovado')
 if media <= 5:
-    print('Aluno reprovado')
-if 5.0 <= media <= 6.9:
-    print('Aluno em exame.')
+    print('Media: %.1f' %media)
+    print('Aluno rerovado')
+if media >= 5.0 and media <= 6.9:
     n5 = float(input())
+    print('Media: %.1f' %media)
+    print('Aluno em exame.')
     print('Nota do exame: %.1f' %n5)
     mediaFinal = (n5 + media) / 2
     if mediaFinal >= 5:
