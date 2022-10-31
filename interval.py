fdd1d505e0cf88422b7e6fcf7cbff6b5f9980c04
Nigraum <yan.corintima@hotmail.com>
2022-10-31 13:25:48 -0300

    Fixing interval.py

--- interval.py
+++ interval.py
@@ -4,10 +4,10 @@ if 0 <= a <= 25:
     print('Intervalo [0,25]')
 
 if 25 < a <= 50:
-    print('Intervalo (25, 50]')
+    print('Intervalo (25,50]')
 
 if 50 < a <= 75:
-    print('Intervalo (50, 75]')
+    print('Intervalo (50,75]')
 
 if 75 < a <= 100:
     print('Intervalo (75,100]')
