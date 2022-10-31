2b0bfa5bc8e98f8e128fa4b88c291c34fc358600
Nigraum <yan.corintima@hotmail.com>
2022-10-31 14:32:45 -0300

    Fixing load.py

--- load.py
+++ load.py
@@ -1,11 +1,8 @@
 while True:
     try:
         list1 = input().split()
-        list2 = input().split()
         x, y = list1
-        a, b = list2
         print(int(x) ^ int(y))
-        print(int(a) ^ int(b))
 
     except EOFError:
         break
