c31f3d38b28f2131cce3e3895476e792505d02d5
Nigraum <yan.corintima@hotmail.com>
2022-11-06 20:54:47 -0300

    Create tabuada2.py

---
+++ tabuada2.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+
+if a > b:
+    print('Nenhuma tabuada no intervalo')
+else:
+    for n in range(a, b + 1):
+        print('-' * 10)
+        for num in range(1, 11):
+            print(f'{n} x {num} = {num * n}')
+
