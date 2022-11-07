3db9454ed2360caf0d1d67b951ebbaead6c0b6cc
Nigraum <yan.corintima@hotmail.com>
2022-11-06 21:40:13 -0300

    Fixing tabuada2.py

--- tabuada2.py
+++ tabuada2.py
@@ -2,10 +2,9 @@ a = int(input())
 b = int(input())
 
 if a > b:
-    print('Nenhuma tabuada no intervalo')
+    print('Nenhuma tabuada no intervalo!')
 else:
     for n in range(a, b + 1):
-        print('-' * 10)
         for num in range(1, 11):
             print(f'{n} x {num} = {num * n}')
-
+        print('-' * 10)
