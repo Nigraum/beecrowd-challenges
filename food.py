d27bad87d27995f6d8eea280a57ea17f693cb6eb
Nigraum <yan.corintima@hotmail.com>
2022-11-12 19:27:13 -0300

    Finishing food.py

--- food.py
+++ food.py
@@ -2,4 +2,16 @@ x = input().split()
 a, b = x
 
 if int(a) == 1:
-    print(f'Total: {4.00 * int(b):.2f}')
+    print(f'Total: R$ {4.00 * int(b):.2f}')
+
+if int(a) == 2:
+    print(f'Total: R$ {4.50 * int(b):.2f}')
+
+if int(a) == 3:
+    print(f'Total: R$ {5.00 * int(b):.2f}')
+
+if int(a) == 4:
+    print(f'Total: R$ {2.00 * int(b):.2f}')
+
+if int(a) == 5:
+    print(f'Total: R$ {1.50 * int(b):.2f}')
