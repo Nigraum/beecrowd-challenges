df40cf8ef60af5b2bfa8f794bfdd6ed8492e3f80
Nigraum <yan.corintima@hotmail.com>
2022-11-15 00:17:36 -0300

    Finishing triangle.py

--- triangle.py
+++ triangle.py
@@ -3,4 +3,10 @@ a, b, c = x
 a = float(a)
 b = float(b)
 c = float(c)
-print(f'valores {a}, {b}, {c}')
+
+Area = (((a + b) / 2) * 2)
+
+if abs(b - c) < a < (b + c) and (a - c) < b < (a + c) and (a - b) < c < (a + b):
+    print(f'Perimetro = {a + b + c}')
+else:
+    print(f'Area = {Area} ')
