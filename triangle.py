016bccb0c67b40c87655446c72c28a19453ddcda
Nigraum <yan.corintima@hotmail.com>
2022-11-15 00:18:57 -0300

    Fixing triangle.py

--- triangle.py
+++ triangle.py
@@ -7,6 +7,6 @@ c = float(c)
 Area = (((a + b) / 2) * 2)
 
 if abs(b - c) < a < (b + c) and (a - c) < b < (a + c) and (a - b) < c < (a + b):
-    print(f'Perimetro = {a + b + c}')
+    print(f'Perimetro = {a + b + c:.1f}')
 else:
-    print(f'Area = {Area} ')
+    print(f'Area = {Area:.1f} ')
